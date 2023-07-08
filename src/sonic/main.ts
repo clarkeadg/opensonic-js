a8a8a2ff7446f667531796ec01a02dd2e02650f8
clarkeadg <clarkeadg@yahoo.com>
2023-07-08 01:48:47 -0700

    docs

--- src/sonic/main.ts
+++ src/sonic/main.ts
@@ -1,24 +1,16 @@
 
 import { engine_init } from "./core/engine"
 
-/**
- * main()
- * Entry point
- */
-export const main = ():void => {
-  engine_init({
-    Xvideo_resolution: "TINY", // TINY NORMAL MAX
-    Xsmooth_graphics: false,   // true false
-    Xfullscreen: false,        // true false
-    Xshow_fps: true,           // true false
-    Xcolor_depth: 32,          // 8 16 24 32
-    Xlevel: null,              // filepath
-    //Xlevel: "data/levels/blue_ocean_3.json",
-    Xquest: null,              // filepath
-    //Xquest: "data/quests/tutorial.json", // this is not done
-    Xlanguage: null,           // filepath
-    //Xlanguage: "data/languages/italiano.json"
-  });
-}
-
-main();
+engine_init({
+  Xvideo_resolution: "TINY", // TINY NORMAL MAX
+  Xsmooth_graphics: false,   // true false
+  Xfullscreen: false,        // true false
+  Xshow_fps: true,           // true false
+  Xcolor_depth: 32,          // 8 16 24 32
+  Xlevel: null,              // filepath
+  //Xlevel: "data/levels/blue_ocean_3.json",
+  Xquest: null,              // filepath
+  //Xquest: "data/quests/tutorial.json", // this is not done
+  Xlanguage: null,           // filepath
+  //Xlanguage: "data/languages/italiano.json"
+});
