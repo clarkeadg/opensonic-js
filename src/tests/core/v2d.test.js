e799a8f80067e38b9103fb028b64a890b4814f8e
clarkeadg <clarkeadg@yahoo.com>
2023-07-07 18:01:13 -0700

    tests

--- src/tests/core/v2d.test.js
+++ src/tests/core/v2d.test.js
@@ -8,7 +8,7 @@ const {
   v2d_rotate,
   v2d_normalize,
   v2d_lerp
-} = require('../sonic/core/v2d');
+} = require('../../sonic/core/v2d');
 
 test('v2d_new', () => {
   const v2d = v2d_new(1,2);
@@ -52,20 +52,31 @@ test('v2d_dotproduct', () => {
   expect(n).toBe(11);
 });
 
-/*test('v2d_rotate', () => {
-  const v2d = v2d_new(1,2);
-  expect(v2d.x).toBe(1);
-  expect(v2d.y).toBe(2);
+test('v2d_rotate', () => {
+  const v2d_1 = v2d_new(1,2);
+  const v2d = v2d_rotate(v2d_1, 1.570796);
+  expect(v2d.x).toBe(-1.9999996732049967);
+  expect(v2d.y).toBe(1.0000006535897397);
 });
 
 test('v2d_normalize', () => {
-  const v2d = v2d_new(1,2);
-  expect(v2d.x).toBe(1);
-  expect(v2d.y).toBe(2);
+  const v2d_1 = v2d_new(1,2);
+  const v2d = v2d_normalize(v2d_1);  
+  expect(v2d.x).toBe(0.4472135954999579);
+  expect(v2d.y).toBe(0.8944271909999159);
+});
+
+test('v2d_normalize 2', () => {
+  const v2d_1 = v2d_new(0,0);
+  const v2d = v2d_normalize(v2d_1);  
+  expect(v2d.x).toBe(0);
+  expect(v2d.y).toBe(0);
 });
 
 test('v2d_lerp', () => {
-  const v2d = v2d_new(1,2);
-  expect(v2d.x).toBe(1);
-  expect(v2d.y).toBe(2);
-});*/
+  const v2d_1 = v2d_new(1,2);
+  const v2d_2 = v2d_new(3,4);
+  const v2d = v2d_lerp(v2d_1, v2d_2, 0.5);
+  expect(v2d.x).toBe(2);
+  expect(v2d.y).toBe(3);
+});
