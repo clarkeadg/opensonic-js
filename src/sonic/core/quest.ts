2a36c054895ad73068e6fdba30f8d4a8c37a2764
clarkeadg <clarkeadg@yahoo.com>
2023-07-23 02:59:14 -0700

    async await

--- src/sonic/core/quest.ts
+++ src/sonic/core/quest.ts
@@ -26,43 +26,16 @@ export interface quest_t {
  * Loads and returns a quest from a file
  * (abs_path must be an ABSOLUTE path)
  */
-export const quest_load = (abs_path:string):Promise<quest_t> => {
+export const quest_load = async (abs_path:string):Promise<quest_t> => {
+  logfile_message(`quest_load("${abs_path}")`); 
 
-  return new Promise(function (fulfill, reject){
-    let q:quest_t = {
-      file: abs_path,
-      name: "",
-      author: "",
-      version: "",
-      description: "",
-      image: null,
-      level_count: 0,
-      levels: [],
-      level_path: [],
-      show_ending: false
-    };
+  const data = await resourcemanager_getJsonFile(abs_path);
+  const questdata = await traverse_quest(<data_quest_t>data);
 
-    logfile_message(`quest_load("${abs_path}")`);
+  /* success! */
+  logfile_message("load_quest() ok!");
 
-    /* default values */
-    
-
-    resourcemanager_getJsonFile(abs_path)
-    .then(traverse_quest)
-    .then(function(questdata:quest_t ){
-
-      if (questdata) {
-        q = questdata;
-      }
-
-      /* success! */
-      logfile_message("load_quest() ok!");
-      //console.log(q)
-
-      fulfill(q);
-    });
-
-   });
+  return questdata;
 }
 
 /*
@@ -85,40 +58,30 @@ export const quest_unload = (qst:quest_t):null => {
 }
 
 /* returns the quest image */
-const load_quest_image = (file:string) => {
-  return new Promise(function (fulfill, reject){
+const load_quest_image = async (file:string) => {
+  const no_image = "data/images/null.png";
+  let s = file ? file : no_image;
+  s = DATA_ROOT + s;
 
-    const no_image = "data/images/null.png";
-    let s = file ? file : no_image;
-    s = DATA_ROOT + s;
-
-    image_load(s)
-    .then(function(img){
-      fulfill(img);
-    });
-  });
+  const img = await image_load(s);
+  return img;
 }
 
 /* interprets a statement from a .json file */
-const traverse_quest = (data:data_quest_t) => {
-  return new Promise(function (fulfill, reject){
-    let q:quest_t = {
-      file: "",
-      name: data.name || "",
-      author: data.author || "Open Sonic",
-      version: data.version || "0",
-      description: data.description || "",
-      show_ending: data.show_ending || false,
-      image: null,
-      levels: data.levels || [],
-      level_count: data.levels ? data.levels.length : 0,
-      level_path: data.levels || []
-    };
-
-    load_quest_image(data.image)
-    .then(function(thumb:HTMLImageElement) {
-      q.image = thumb;
-      fulfill(q);
-    });
-  });
+const traverse_quest = async (data:data_quest_t) => {
+  const q:quest_t = {
+    file: "",
+    name: data.name || "",
+    author: data.author || "Open Sonic",
+    version: data.version || "0",
+    description: data.description || "",
+    show_ending: data.show_ending || false,
+    image: null,
+    levels: data.levels || [],
+    level_count: data.levels ? data.levels.length : 0,
+    level_path: data.levels || []
+  };
+  const thumb = await load_quest_image(data.image);
+  q.image = <HTMLImageElement>thumb;
+  return(q);
 }
